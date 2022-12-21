1d744aff38bc1e8eebb07a124f96be87116277dc
Daniel Roos <daniel.roos@madepeople.se>
2022-12-21 20:57:34 +0100

    More links

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,20 @@
 import { A } from "solid-start";
-import LinkedIn from "~/icons/LinkedIn";
+import { LinkedIn, GitHub, Email } from "~/icons";
 
 export default function Home() {
   return (
     <main class="flex flex-col items-center justify-between h-full">
       <h1 class="text-white text-center text-5xl">Daniel Roos</h1>
-      <footer>
-        <A href="https://www.linkedin.com/in/daniel-roos-a84b99138/">
+      <footer class="flex gap-8">
+        <a href="https://www.linkedin.com/in/daniel-roos-a84b99138/">
           <LinkedIn />
-        </A>
+        </a>
+        <a href="https://github.com/RoosDaniel/">
+          <GitHub />
+        </a>
+        <a href="mailto:96danielroos@gmail.com">
+          <Email />
+        </a>
       </footer>
     </main>
   );
